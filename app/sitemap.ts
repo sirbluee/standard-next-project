0f411135100c0eb3355d5aa88456f763310bd4bd
sirblue <sophearum14@gmail.com>
2024-08-13 13:29:58 +0700

    added sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://standard-next-project.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://standard-next-project.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://standard-next-project.vercel.app/services',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
